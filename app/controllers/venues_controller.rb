c176ee664d7097835cdc55e4f3856a7410c0018e
Daniela Garcia <danielagarcia13.99@gmail.com>
2024-10-02 15:39:36 +0000

    fixed querying error in comment model

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -9,9 +9,8 @@ class VenuesController < ApplicationController
 
   def show
     venue_id = params.fetch("venue_id")
-    matching_venues = Venue.where({ :id => venue_id }).at(0)
-    @the_venue = matching_venues
-
+    matching_venues = Venue.where({ :id => venue_id })
+    @the_venue = matching_venues.at(0)
     render({ :template => "venue_templates/details" })
   end
 
@@ -21,14 +20,12 @@ class VenuesController < ApplicationController
     @venue.name = params.fetch("query_name")
     @venue.neighborhood = params.fetch("query_neighborhood")
     @venue.save
-
     redirect_to("/venues/#{@venue.name}")
   end
   
   def update
-    the_id = params.fetch("venue_id")
-
-    @venue = Venue.where({ :id => the_id })
+    the_id = params.fetch("the_id")
+    @venue = Venue.where({ :id => the_id }).at(0)
     @venue.address = params.fetch("query_address")
     @venue.name = params.fetch("query_name")
     @venue.neighborhood = params.fetch("query_neighborhood")
